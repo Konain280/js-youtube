2822e72e1cd6fdae94b23f219332f8597836d30c
Konain280 <konaintahir22@gmail.com>
2026-07-12 02:34:31 +0500

    Add result.js

---
+++ result.js
@@ -0,0 +1,14 @@
+let number=prompt("Enter a number:");
+if(number>90){
+    console.log("A");
+}if(number>80){
+    console.log("B");
+}if(number>70){
+    console.log("C");
+}if(number>60){
+    console.log("D");
+}if(number>50){
+    console.log("E");
+}if(number>40){
+    console.log("F");
+}
