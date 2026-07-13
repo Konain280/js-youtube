47d36f16e239d37757c02621d155239951b0d626
Konain280 <konaintahir22@gmail.com>
2026-07-14 01:15:08 +0500

    Resolved merge conflict

--- switch.js
+++ switch.js
@@ -5,7 +5,11 @@ switch(day){
         console.log("Start of the work week hehehehe");
         break;      
     case "Tuesday":
+<<<<<<< Updated upstream
         console.log("Second day of the work week hahahahahahahahaa");
+=======
+        console.log("Second day of the work week wefhuweneufhewufuewhuehuwh");
+>>>>>>> Stashed changes
         break;
     case "Wednesday":
         console.log("Midweek day");
